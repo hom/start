1e34e085249138a77d3999a70173a4cf5c5e91c3
mengjun <813935532@qq.com>
2019-04-11 19:54:21 +0800

    add add-mark panel

--- src/components/card-item.vue
+++ src/components/card-item.vue
@@ -2,24 +2,46 @@
   <el-card class="box-card">
   <div slot="header" class="clearfix">
     <span>{{ card.title }}</span>
-    <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+    <el-button style="float: right; padding: 3px 0" type="text" @click="isShowAddMark = true">添加</el-button>
   </div>
   <el-button @click="redirect(mark)" class="item-button" v-for="(mark, index) in card.marks" :key="index" round>
     <div class="favicon"><img :src="mark.favicon" alt=""></div>
     {{ mark.title }}
   </el-button>
-  <!-- <a class="mark-link" v-for="(mark, index) in card.marks" :key="index" :href="mark.target">
-    <div class="favicon"><img :src="mark.favicon" alt=""></div>
-    {{ mark.title }}
-  </a> -->
+  <el-dialog title="收货地址" :visible.sync="isShowAddMark">
+    <el-form :model="form">
+      <el-form-item label="标题" :label-width="formLabelWidth">
+        <el-input v-model="form.title" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="图标" :label-width="formLabelWidth">
+        <el-input v-model="form.favicon" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="链接地址" :label-width="formLabelWidth">
+        <el-input v-model="form.target" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="面板" :label-width="formLabelWidth">
+        <el-select class="el-select-card" v-model="form.card" placeholder="请选择活动区域">
+          <el-option v-for="(card, index) in cards" :key="index" :label="card.title" :value="card.objectId"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="isShowAddMark = false">取 消</el-button>
+      <el-button type="primary" @click="isShowAddMark = false">确 定</el-button>
+    </div>
+  </el-dialog>
 </el-card>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {
-      items: Math.floor(Math.random() * 10),
+      form: '',
+      formLabelWidth: '120px',
+      isShowAddMark: false,
     };
   },
   props: {
@@ -29,6 +51,11 @@ export default {
         return {};
       },
     }
+  },
+  computed: {
+    ...mapState({
+      cards: (state) => state.cards
+    }),
   },
   methods: {
     redirect(mark) {
@@ -82,5 +109,9 @@ export default {
   .box-card {
     display: inline-block;
     min-height: 60px;
+  }
+
+  .el-select-card {
+    float: left;
   }
 </style>
